4a8152bdcc9c345741003beef55f489a071c4a85
karyna.yun <karyna.yun@educa.jcyl.es>
2024-10-25 09:32:16 +0200

    Ejercicio2.py

---
+++ src/PythonBasico/Ejercicio2.py
@@ -0,0 +1,5 @@
+nombre_completo = input("Introduce tu nombre completo: ")
+
+print("En minúsculas:", nombre_completo.lower())
+print("En mayúsculas:", nombre_completo.upper())
+print("Con iniciales en mayúscula:", nombre_completo.title())
